4fbcbf39287baecc418454f562d52a86aab0a8ec
sebug <sebastian.gfeller@gmail.com>
2015-11-22 17:25:51 +0100

    Getting to draw

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,19 @@ import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Data.Maybe
+
+import Graphics.Canvas (getCanvasElementById, getContext2D, Canvas())
+import Graphics.Canvas.Free
+
+main :: forall e. Eff (console :: CONSOLE, canvas :: Canvas | e) Unit
 main = do
-  log "Replace this with canvas later."
+  canvas <- getCanvasElementById "d"
+  case canvas of
+    Nothing -> log "Oh no, canvas not found"
+    Just c -> do
+      context <- getContext2D c
+      runGraphics context $ do
+        setFillStyle "#000000"
+        rect { x: 0.0, y: 0.0, w: 400.0, h: 600.0 }
+        fill
