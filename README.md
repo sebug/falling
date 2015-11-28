cd58bc1abf146c238604d1636d2bb21034c31ea0
sebug <sebastian.gfeller@gmail.com>
2015-11-28 14:32:29 +0100

    Excuses

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 When at a concert Friday, I saw a computer animation with various 2d shapes
 falling down, changing size according to the rythm of the music.
 
-I want to try to replicate the former.
+I want to try to replicate the former, but patience, I'm lacking the focus right now, so no functional (heh) code.
 
 To use:
 
--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,13 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
+import Prelude (Unit(),bind,($))
+import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Console (log, CONSOLE())
 
-import Data.Maybe
+import Data.Maybe (Maybe(..))
 
-import Graphics.Canvas (getCanvasElementById, getContext2D, Canvas())
-import Graphics.Canvas.Free
+import Graphics.Canvas (getCanvasElementById, getContext2D, Canvas(), setCanvasWidth, setCanvasHeight)
+import Graphics.Canvas.Free (runGraphics, setFillStyle, rect, fill)
 
 main :: forall e. Eff (console :: CONSOLE, canvas :: Canvas | e) Unit
 main = do
@@ -18,5 +18,5 @@ main = do
       context <- getContext2D c
       runGraphics context $ do
         setFillStyle "#000000"
-        rect { x: 0.0, y: 0.0, w: 400.0, h: 600.0 }
+        rect { x: 100.0, y: 100.0, w: 400.0, h: 600.0 }
         fill
